9fdab4069171f618084333cd07cdf4981e5d1519
Will <wor101@gmail.com>
2021-12-29 17:58:36 -0500

    Dijkstras algorithm in Golang

---
+++ chapter18/graphs.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Queue struct {
+	data []*Vertex
+}
+
+func (q *Queue) enqueue(vertex *Vertex) {
+	q.data = append(q.data, vertex)
+}
+
+func (q *Queue) dequeue() *Vertex {
+	dequeued := q.data[0]
+	q.data = q.data[1:]
+	return dequeued
+}
+
+func (q *Queue) read() *Vertex {
+	return q.data[0]
+}
+
+type Vertex struct {
+	value             string
+	adjacent_vertices []*Vertex
+}
+
+func (v *Vertex) add_adjacent_vertex(vertex *Vertex) {
+	for _, adjacent_vertex := range v.adjacent_vertices {
+		if reflect.DeepEqual(vertex, adjacent_vertex) {
+			return
+		}
+	}
+
+	v.adjacent_vertices = append(v.adjacent_vertices, vertex)
+	vertex.add_adjacent_vertex(v)
+}
+
+func (v *Vertex) print_all_adjacent() {
+	for index, vertex := range v.adjacent_vertices {
+		fmt.Printf("Index %v: %v", index, vertex)
+		fmt.Println("")
+	}
+}
+
+func (v *Vertex) dfs_traverse(vertex *Vertex, viseted_vertices map[string]bool) {
+	viseted_vertices[vertex.value] = true
+
+	fmt.Println(vertex.value)
+	fmt.Println(viseted_vertices)
+
+	for _, adjacent_vertex := range vertex.adjacent_vertices {
+		if _, ok := viseted_vertices[adjacent_vertex.value]; ok {
+			continue
+		}
+		v.dfs_traverse(adjacent_vertex, viseted_vertices)
+	}
+}
+
+func (v *Vertex) dfs(vertex *Vertex, search_value string, viseted_vertices map[string]bool) *Vertex {
+	if vertex.value == search_value {
+		return vertex
+	}
+
+	viseted_vertices[vertex.value] = true
+
+	for _, adjacent_vertex := range vertex.adjacent_vertices {
+		// skip to next vertex if adjacent has already been visited
+		if _, ok := viseted_vertices[adjacent_vertex.value]; ok {
+			continue
+		}
+
+		// return adjacent if its value is the one we are looking for
+		if adjacent_vertex.value == search_value {
+			return adjacent_vertex
+		}
+
+		// trie to find it using recursion
+		vertex_were_searching_for := v.dfs(adjacent_vertex, search_value, viseted_vertices)
+
+		if vertex_were_searching_for != nil {
+			return vertex_were_searching_for
+		}
+	}
+	return nil
+}
+
+func (v *Vertex) bfs_traverse(starting_vertex *Vertex) {
+	queue := Queue{make([]*Vertex, 0)}
+
+	visited_vertices := make(map[string]bool)
+	visited_vertices[starting_vertex.value] = true
+	queue.enqueue(starting_vertex)
+
+	for {
+		if len(queue.data) <= 0 {
+			break
+		}
+		current_vertex := queue.dequeue()
+
+		fmt.Println(current_vertex)
+
+		for _, adjacent_vertex := range current_vertex.adjacent_vertices {
+			if _, ok := visited_vertices[adjacent_vertex.value]; ok {
+				continue
+			}
+			visited_vertices[adjacent_vertex.value] = true
+			queue.enqueue(adjacent_vertex)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Welcome to Graphs!")
+
+	will := Vertex{"Will", make([]*Vertex, 0)}
+	theon := Vertex{"Theon", make([]*Vertex, 0)}
+	morgar := Vertex{"Morgar", make([]*Vertex, 0)}
+
+	fmt.Println("add_adjacent_vertex method:")
+	will.add_adjacent_vertex(&theon)
+	will.add_adjacent_vertex(&theon)
+	will.add_adjacent_vertex(&morgar)
+	fmt.Println("WillAdjacent:")
+	will.print_all_adjacent()
+	fmt.Println("")
+	fmt.Println("TheonAdjacent:")
+	theon.print_all_adjacent()
+	fmt.Println("")
+	fmt.Println("MorgarAdjacent:")
+	morgar.print_all_adjacent()
+	fmt.Println("")
+
+	fmt.Println("dfs_traverse method:")
+	will.dfs_traverse(&will, make(map[string]bool))
+	fmt.Println("")
+
+	fmt.Println("dfs search method:")
+	searchNode := will.dfs(&will, "Morgar", make(map[string]bool))
+	fmt.Println(searchNode)
+	fmt.Println("")
+
+	fmt.Println("bfs_traverse method:")
+	will.bfs_traverse(&will)
+}
---
+++ chapter18/weightedGraph.go
@@ -0,0 +1,137 @@
+package main
+
+import "fmt"
+
+type WeightedGraphVertex struct {
+	value             string
+	adjacent_vertices map[*WeightedGraphVertex]int
+}
+
+func (wgv *WeightedGraphVertex) add_adjacent_vertex(vertex *WeightedGraphVertex, weight int) {
+	wgv.adjacent_vertices[vertex] = weight
+}
+
+func newWeightedGraphVertex(name string) WeightedGraphVertex {
+	return WeightedGraphVertex{name, make(map[*WeightedGraphVertex]int)}
+}
+
+func filterSlice(unvisited_cities []*WeightedGraphVertex, current_city *WeightedGraphVertex) []*WeightedGraphVertex {
+	filtered_cities := make([]*WeightedGraphVertex, 0)
+
+	for _, city := range unvisited_cities {
+		if city.value != current_city.value {
+			filtered_cities = append(filtered_cities, city)
+		}
+	}
+	return filtered_cities
+}
+
+func cheapestUnvisited(cities []*WeightedGraphVertex, cheapest_prices_table map[string]int) *WeightedGraphVertex {
+	if len(cities) <= 0 {
+		return nil
+	}
+	cheapestCity := cities[0]
+
+	for _, city := range cities {
+		currentLow := cheapest_prices_table[cheapestCity.value]
+		cityCost := cheapest_prices_table[city.value]
+
+		if cityCost < currentLow {
+			cheapestCity = city
+		}
+	}
+	return cheapestCity
+}
+
+func dijkstra_shortest_path(starting_city *WeightedGraphVertex, final_destination *WeightedGraphVertex) []string {
+	cheapest_prices_table := make(map[string]int)
+	cheapest_previous_stopover_city_table := make(map[string]string)
+
+	unvisited_cities := make([]*WeightedGraphVertex, 0)
+
+	visited_cities := make(map[string]bool)
+
+	cheapest_prices_table[starting_city.value] = 0
+
+	current_city := starting_city
+
+	for {
+		if current_city == nil {
+			break
+		}
+
+		visited_cities[current_city.value] = true
+		unvisited_cities = filterSlice(unvisited_cities, current_city)
+
+		//fmt.Println(current_city.adjacent_vertices)
+		for adjacent_city, price := range current_city.adjacent_vertices {
+			// add to list of unvisited cities if it is not in the list of visited
+			if _, ok := visited_cities[adjacent_city.value]; !ok {
+				unvisited_cities = append(unvisited_cities, adjacent_city)
+			}
+
+			price_through_current_city := cheapest_prices_table[current_city.value] + price
+
+			// if price from starting city to adjacent city is cheapest so far
+			if _, ok := cheapest_prices_table[adjacent_city.value]; !ok || price_through_current_city < cheapest_prices_table[adjacent_city.value] {
+
+				//update two tables
+				cheapest_prices_table[adjacent_city.value] = price_through_current_city
+				cheapest_previous_stopover_city_table[adjacent_city.value] = current_city.value
+			}
+
+		}
+		//vist next unvistied city, choosing the cheapest to get to from STARTING CITY
+		current_city = cheapestUnvisited(unvisited_cities, cheapest_prices_table)
+	}
+
+	fmt.Println("Cheapest Price Table:", cheapest_prices_table)
+	fmt.Println("Cheapest Stopover Table:", cheapest_previous_stopover_city_table)
+
+	// create route path
+
+	shortest_path := make([]string, 0)
+
+	current_city_name := final_destination.value
+
+	for {
+		if current_city_name == starting_city.value {
+			break
+		}
+		shortest_path = append(shortest_path, current_city_name)
+		current_city_name = cheapest_previous_stopover_city_table[current_city_name]
+	}
+	shortest_path = append(shortest_path, starting_city.value)
+
+	return shortest_path
+}
+
+func main() {
+
+	denver := newWeightedGraphVertex("Denver")
+	elpaso := newWeightedGraphVertex("El Paso")
+	chicago := newWeightedGraphVertex("Chicago")
+	atlanta := newWeightedGraphVertex("Atlanta")
+	boston := newWeightedGraphVertex("Boston")
+
+	// add adjacent
+	//fmt.Println("add_adjacent_vertex method:")
+	atlanta.add_adjacent_vertex(&denver, 160)
+	atlanta.add_adjacent_vertex(&boston, 100)
+	boston.add_adjacent_vertex(&denver, 180)
+	boston.add_adjacent_vertex(&chicago, 120)
+	chicago.add_adjacent_vertex(&elpaso, 80)
+	denver.add_adjacent_vertex(&chicago, 40)
+	denver.add_adjacent_vertex(&elpaso, 140)
+	elpaso.add_adjacent_vertex(&boston, 100)
+
+	// fmt.Println(denver)
+	// fmt.Println(elpaso)
+	// fmt.Println(chicago)
+	// fmt.Println(atlanta)
+	// fmt.Println(boston)
+	// fmt.Println("")
+
+	fmt.Println(dijkstra_shortest_path(&atlanta, &elpaso))
+
+}
